25d4065291b554c1288bfade5fdae6c505a0e5be
21shield <21netaly@gmail.com>
2020-06-12 15:59:10 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}
+  data.each do |key, value|  
+    value.each do |inner_key, inner_value|
+      inner_value.each do |element|
+        if !organized_data[element] 
+          organized_data[element]= {}
+        end
+        if !organized_data[element][key]
+          organized_data[element][key] = []
+        end
+          organized_data[element][key] << inner_key.to_s
+      end
+    end
+  end
+  p organized_data
 end
+
